28758bf173b6abe465110f7f92b173c972207ed0
chukaonline <115892412+chukaonline@users.noreply.github.com>
2026-04-16 11:45:08 +0100

    Update const.py
    Adding option for region selection

--- custom_components/alpsolar_inteless/const.py
+++ custom_components/alpsolar_inteless/const.py
@@ -1,3 +1,10 @@
 DOMAIN = "alpsolar_inteless"
 CONF_PLANT_ID = "plant_id"
-BASE_URL = "https://euapi.inteless.com"
+CONF_REGION = "region"
+
+REGIONS = {
+    "Europe": "https://euapi.inteless.com",
+    "America": "https://usapi.inteless.com",
+    "Asia": "https://asiaapi.inteless.com",
+    "Global": "https://api.inteless.com"
+}
