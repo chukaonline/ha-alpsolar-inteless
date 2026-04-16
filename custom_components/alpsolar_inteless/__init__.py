5952497f576b7b240d78e0d29b8251e9c9823f7d
chukaonline <115892412+chukaonline@users.noreply.github.com>
2026-04-16 11:30:51 +0100

    Update __init__.py

--- custom_components/alpsolar_inteless/__init__.py
+++ custom_components/alpsolar_inteless/__init__.py
@@ -2,9 +2,8 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass, entry):
     """Set up Alpsolar Inteless from a config entry."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+    # Note the 's' at the end of setups and the [brackets] around "sensor"
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
